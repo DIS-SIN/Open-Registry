44b38b61f7f44369fd55a2a7a57a0df9fb60c85f
Sinan Baltacioglu <siphon@gmail.com>
2019-05-14 18:27:39 -0400

    Switched from MDL to MaterializeCSS

--- static/src/App.js
+++ static/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
 import './App.css';
 
 const la_streams = [
@@ -12,37 +13,28 @@ const la_streams = [
 
 const ControlStreamSelector = () => {
   const ctrl = la_streams.map((item, index) => (
-    <label key={"ctrl_stream_" + index} className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" htmlFor={"ctrl_stream_cb_" + index}>
-      <input type="checkbox" name="ctrl_stream_cb" id={"ctrl_stream_cb_" + index} className="mdl-checkbox__input" />
-      <span className="mdl-checkbox__label">{item.stream}</span>
-    </label>
+    <span key={"ctrl_stream_" + index} className="p-inline-cb">
+      <label htmlFor={"ctrl_stream_cb_" + index}>
+        <input type="checkbox" name="ctrl_stream_cb" id={"ctrl_stream_cb_" + index} />
+        <span>{item.stream}</span>
+      </label>
+    </span>
   ));
   return (
-    <div className="mdl-grid">
-      <div className="mdl-cell mdl-cell--12-col">
-        <h5>Category</h5>
-        <h6>Select which categories the content belongs to</h6>
-        {ctrl}
-      </div>
+    <div>
+      <h5><i className="material-icons">done_all</i> What is it related to?</h5>
+      {ctrl}
     </div>
   );
 }
 
 const ControlURLInput = () => {
   return (
-    <div className="mdl-grid">
-      <div className="mdl-layout-spacer"></div>
-      <div className="mdl-cell mdl-cell--12-col">
-        <h5>Add Content to Digital Open Learning</h5>
-        <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-          <input className="mdl-textfield__input"
-            id="content_URL" type="text" name="url" placeholder="Paste your content URL here." required
-            pattern="https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
-          />
-          <label className="mdl-textfield__label" htmlFor="content_URL">Content URL</label>
-        </div>
+    <div>
+      <div className="input-field">
+        <h5><i className="material-icons">add</i> Add Content to Digital Open Learning</h5>
+        <input id="content_URL" type="text" name="url" placeholder="Paste your content URL here." />
       </div>
-      <div className="mdl-layout-spacer"></div>
     </div>
   );
 }
@@ -50,24 +42,24 @@ const ControlURLInput = () => {
 const ControlURLSubmit = props => {
   const { btn } = props;
   return (
-    <div className="mdl-grid">
-      <div className="mdl-cell mdl-cell--12-col mdl-cell--12-col-phone">
-        <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-          {btn}
-        </button>
-      </div>
-    </div>
+    <button id="btnSubmit" className="btn btn-large waves-effect teal darken-2" type="submit">
+      {btn}
+    </button>
   )
 }
 
 const Header = props => {
   const { title } = props;
   return (
-    <header className="mdl-layout__header">
-      <div className="mdl-layout__header-row">
-        <span className="mdl-layout-title">{title}</span>
-        <div className="mdl-layout-spacer"></div>
-      </div>
+    <header>
+      <nav>
+        <div className="nav-wrapper grey darken-4">
+          <a href="#!" className="brand-logo center">{title}</a>
+          <ul id="nav-mobile" className="left hide-on-med-and-down">
+            <li><a href="#!">Digital Open Learning</a></li>
+          </ul>
+        </div>
+      </nav>
     </header>
   )
 }
@@ -75,24 +67,30 @@ const Header = props => {
 const Layout = props => {
   return (
     <div className="App">
-      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-        <Header title="OpenRegistry" />
-        <main className="mdl-layout__content">
-          <div className="mdl-grid">
-            <div className="mdl-layout-spacer"></div>
-            <div className="mdl-cell mdl-cell--6-col">
-              <div className="page-content">
-                <form id="form">
-                  <ControlURLInput />
-                  <ControlStreamSelector />
-                  <ControlURLSubmit btn="Submit" />
-                </form>
-              </div>
+      <Header title="OpenRegistry" />
+      <main>
+        <div className="container">
+          <div className="card">
+            <div className="card-content">
+              <form id="form">
+                <div className="row">
+                  <div className="col s12 m12">
+                    <ControlURLInput />
+                  </div>
+                  <div className="col s12 m12">
+                    <ControlStreamSelector />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col s12 m12 center">
+                    <ControlURLSubmit btn="Send to DOL" />
+                  </div>
+                </div>
+              </form>
             </div>
-            <div className="mdl-layout-spacer"></div>
           </div>
-        </main>
-      </div>
+        </div>
+      </main>
     </div>
   );
 }
